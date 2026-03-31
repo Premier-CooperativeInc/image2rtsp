1653c3f16cc582954425fdc9e33bbef8d4aaf7d1
Vikram Raj <54244791+nvikramraj@users.noreply.github.com>
2026-03-31 10:49:54 -0500

    Update image2rtsp.launch.py

--- launch/image2rtsp.launch.py
+++ launch/image2rtsp.launch.py
@@ -1,33 +1,32 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-
     bringup_share = get_package_share_directory('image2rtsp')
     default_config_dir = os.path.join(bringup_share, 'config')
 
+    # Declare the argument
     config_dir_arg = DeclareLaunchArgument(
-        'config',
+        'config_dir',
         default_value=default_config_dir,
         description='Path to config directory'
     )
 
-    config_dir = LaunchConfiguration('config')
-    stream_file = PathJoinSubstitution([config_dir, 'parameters.yaml'])
-
+    # LaunchConfiguration holds the path
+    config_dir = LaunchConfiguration('config_dir')
+    parameters_file = os.path.join(default_config_dir, 'parameters.yaml')  # fallback
 
     stream_node = Node(
         package='image2rtsp',
         executable='image2rtsp',
         name='image2rtsp',
-        parameters=[stream_file],
+        parameters=[parameters_file],  # pass string path here
         output='log'
     )
-
 
     return LaunchDescription([
         config_dir_arg,
