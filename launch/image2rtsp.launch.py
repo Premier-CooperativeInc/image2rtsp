26591490608334e923d8b571b602ecb7735eb63b
Vikram Raj <54244791+nvikramraj@users.noreply.github.com>
2026-03-31 10:59:55 -0500

    Update image2rtsp.launch.py

--- launch/image2rtsp.launch.py
+++ launch/image2rtsp.launch.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
