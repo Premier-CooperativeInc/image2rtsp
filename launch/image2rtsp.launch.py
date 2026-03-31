49de9e259eac6619e501dc85339210f9e18fa9b6
Vikram Raj <54244791+nvikramraj@users.noreply.github.com>
2026-03-31 08:46:23 -0500

    Update image2rtsp.launch.py
    Included option to use external launch files.

--- launch/image2rtsp.launch.py
+++ launch/image2rtsp.launch.py
@@ -1,20 +1,35 @@
-import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
-   config = os.path.join(
-      get_package_share_directory('image2rtsp'),
-      'config',
-      'parameters.yaml'
-      )
 
-   return LaunchDescription([
-      Node(
-         package='image2rtsp',
-         executable='image2rtsp',
-         name='image2rtsp',
-         parameters=[config]
-      )
-   ])
+    bringup_share = get_package_share_directory('image2rtsp')
+    default_config_dir = os.path.join(bringup_share, 'config')
+
+    config_dir_arg = DeclareLaunchArgument(
+        'config',
+        default_value=default_config_dir,
+        description='Path to config directory'
+    )
+
+    config_dir = LaunchConfiguration('config')
+    stream_file = PathJoinSubstitution([config_dir, 'parameters.yaml'])
+
+
+    stream_node = Node(
+        package='image2rtsp',
+        executable='image2rtsp',
+        name='image2rtsp',
+        parameters=[stream_file],
+        output='log'
+    )
+
+
+    return LaunchDescription([
+        config_dir_arg,
+        stream_node
+    ])
